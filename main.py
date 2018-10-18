ff2bba59e13c17fd7c1e623cffc9b77efe124d63
Brandon Berhent <bbedward@gmail.com>
2018-10-18 09:49:54 -0400

    Fix pre-caching, pause pre-caching while requests are coming in

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ try:
 except Exception:
     pass
 
-PRECACHE_Q_KEY='betsy_precache_queue'
-
 ### PEER-related functions
 
 async def json_get(url, request, timeout=settings.TIMEOUT):
@@ -40,12 +38,13 @@ async def work_cancel(hash):
     request = {"action":"work_cancel", "hash":hash}
     tasks = []
     for p in settings.WORK_SERVERS:
-        tasks.append(json_get(p, request))
+        if '?key' not in p: # dPOW doesn't support work_cancel
+            tasks.append(json_get(p, request))
     # Don't care about waiting for any responses on work_cancel
     for t in tasks:
         asyncio.ensure_future(t)
 
-async def work_generate(hash):
+async def work_generate(hash, redis):
     """RPC work_generate"""
     request = {"action":"work_generate", "hash":hash}
     tasks = []
@@ -62,25 +61,32 @@ async def work_generate(hash):
         done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
         for task in done:
             result = task.result()
-            if result is not None:
+            if result is not None and 'work' in result:
                 asyncio.ensure_future(work_cancel(hash))
+                await redis.set(hash, result['work'], expire=600000) # Cache work
                 return task.result()
     # Fallback method
     if settings.NODE_FALLBACK and NODE_URL and NODE_PORT:
         return await json_get(f"http://{NODE_URL}:{NODE_PORT}", request, timeout=300)
     return None
 
-async def precache_queue_process(app):
+async def precache_queue_process(app, queue):
     while True:
-        await asyncio.sleep(5)
-        item = await app['redis'].lpop(PRECACHE_Q_KEY)
-        if item is None:
+        if app['busy']:
+            # Wait and try precache again later
+            asyncio.sleep(5)
             continue
-        log.server_logger.info(f"precaching {str(item)}")
-        work_response = await work_generate(str(item))
-        if work_response is None or 'work' not in work_response:
+
+        # Pop item off the queue (blocking)
+        hash = str(await queue.get())
+        if hash is None:
             continue
-        await app['redis'].set(str(item), work_response['work'], expire=600000)
+        # See if already have this hash cached
+        have_pow = await app['redis'].get(hash)
+        if have_pow is not None:
+            continue # Already cached
+        log.server_logger.info(f"precaching {hash}")
+        await work_generate(hash, app['redis'])
 
 ### END PEER-related functions
 
@@ -100,12 +106,18 @@ async def rpc(request):
         return web.json_response({"work":work})
 
     # Not in cache, request it from peers
-    respjson = await work_generate(requestjson['hash'])
-    if respjson is None:
-        return web.HTTPError(reason="Couldn't generate work")
-    else:
-        await request.app['redis'].set(requestjson['hash'], respjson['work'], expire=600000)
-    return web.json_response(respjson)
+    try:
+        request.app['busy'] = True # Halts the precaching process
+        respjson = await work_generate(requestjson['hash'], request.app['redis'])
+        if respjson is None:
+            request.app['busy'] = False
+            return web.HTTPError(reason="Couldn't generate work")
+        request.app['busy'] = False
+        return web.json_response(respjson)
+    except Exception as e:
+        request.app['busy'] = False
+        log.server_logger.error(e)
+        return web.HTTPServerError()
 
 async def callback(request):
     requestjson = await request.json()
@@ -117,9 +129,6 @@ async def callback(request):
     # Precache POW if necessary
     if not settings.PRECACHE:
         return web.Response(status=200)
-    have_pow = await request.app['redis'].get(hash)
-    if have_pow is not None:
-        return web.Response(status=200) # Already computed POW for this hash
     block = json.loads(requestjson['block'])
     if 'previous' not in block:
         log.server_logger.info(f"previous not in block from callback {str(block)}")
@@ -128,7 +137,7 @@ async def callback(request):
     if previous_pow is None:
         return web.Response(status=200) # They've never requested work from us before so we don't care
     else:
-        await request.app['redis'].rpush(PRECACHE_Q_KEY, block['previous'])
+        await request.app['precache_queue'].put(hash)
     return web.Response(status=200)
 
 ### END API
@@ -149,7 +158,8 @@ async def get_app():
 
     async def init_queue(app):
         """Initialize task queue"""
-        app['precache_task'] = app.loop.create_task(precache_queue_process(app))
+        app['precache_queue'] = asyncio.Queue(loop=app.loop)
+        app['precache_task'] = app.loop.create_task(precache_queue_process(app, app['precache_queue']))
 
     async def clear_queue(app):
         app['precache_task'].cancel()
@@ -160,6 +170,7 @@ async def get_app():
     else:
         logging.basicConfig(level='INFO')
     app = web.Application()
+    app['busy'] = False
     app.add_routes([web.post('/', rpc)])
     app.add_routes([web.post('/callback', callback)])
     app.on_startup.append(open_redis)
