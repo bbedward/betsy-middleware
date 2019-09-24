80f796e398263054fccfd603e34955203cbcd3ac
BB <bbedward@gmail.com>
2019-09-24 15:18:26 -0400

    Fix clientsession leak

--- dpow_wsclient.py
+++ dpow_wsclient.py
@@ -21,25 +21,25 @@ class DPOWClient():
 
     async def open_connection(self):
         """Create the websocket connection to dPOW service"""
-        session = ClientSession()
-        async with session.ws_connect(self.dpow_url) as ws:
-            self.ws = ws
-            async for msg in ws:
-                if msg.type == WSMsgType.TEXT:
-                    if msg.data == 'close':
-                        await ws.close()
-                    else:
-                        # Handle Reply
-                        log.server_logger.debug(f'WS Message Received {msg.data}')
-                        msg_json = json.loads(msg.data)
-                        await self.app['redis'].rpush(f'dpow_{msg_json["id"]}', msg.data)
-                        await self.app['redis'].expire(f'dpow_{msg_json["id"]}', 60)
-                elif msg.type == WSMsgType.CLOSE:
-                    log.server_logger.info('WS Connection closed normally')
-                    break
-                elif msg.type == WSMsgType.ERROR:
-                    log.server_logger.info('WS Connection closed with error %s', ws.exception())
-                    break
+        async with ClientSession() as session:
+            async with session.ws_connect(self.dpow_url) as ws:
+                self.ws = ws
+                async for msg in ws:
+                    if msg.type == WSMsgType.TEXT:
+                        if msg.data == 'close':
+                            await ws.close()
+                        else:
+                            # Handle Reply
+                            log.server_logger.debug(f'WS Message Received {msg.data}')
+                            msg_json = json.loads(msg.data)
+                            await self.app['redis'].rpush(f'dpow_{msg_json["id"]}', msg.data)
+                            await self.app['redis'].expire(f'dpow_{msg_json["id"]}', 60)
+                    elif msg.type == WSMsgType.CLOSE:
+                        log.server_logger.info('WS Connection closed normally')
+                        break
+                    elif msg.type == WSMsgType.ERROR:
+                        log.server_logger.info('WS Connection closed with error %s', ws.exception())
+                        break
 
     async def get_id(self) -> int:
         """Get ID that should be used for this request"""
